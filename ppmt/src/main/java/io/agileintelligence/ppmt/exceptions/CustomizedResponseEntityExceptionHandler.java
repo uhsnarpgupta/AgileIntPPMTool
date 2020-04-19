7c4ffa9a1f7a0e51ea17c03b34b7195cff142302
pranshu <uhsnarpggupta@gmail.com>
2020-04-19 17:26:59 +0530

    Added project files

--- ppmt/src/main/java/io/agileintelligence/ppmt/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ ppmt/src/main/java/io/agileintelligence/ppmt/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,5 @@
 package io.agileintelligence.ppmt.exceptions;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,16 +13,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
-    public final ResponseEntity<Object> handleProjectIdAlreadyExistsException(ProjectIdException ex, WebRequest request){
+    public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest request) {
         ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
-
     }
 
     @ExceptionHandler
-    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request){
+    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request) {
         ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex, WebRequest request) {
+        UsernameAlreadyExistsResponse exceptionResponse = new UsernameAlreadyExistsResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
--- ppmt/src/main/java/io/agileintelligence/ppmt/repository/ProjectRepository.java
+++ ppmt/src/main/java/io/agileintelligence/ppmt/repository/ProjectRepository.java
@@ -1,15 +1,15 @@
 package io.agileintelligence.ppmt.repository;
 
-
 import io.agileintelligence.ppmt.domain.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+    Project findByProjectIdentifier(String projectId);
 
-    Project findByProjectIdentifier(String projectid);
+    @Override
     Iterable<Project> findAll();
 
-
+    Iterable<Project> findAllByProjectLeader(String username);
 }
